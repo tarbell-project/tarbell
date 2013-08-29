cc6b4dda7a5f172eb10df43b83b00040b076d0f3
David Eads <davideads@gmail.com>
2013-08-29 02:08:09 -0500

    toward a working runserver

--- tarbell/cli.py
+++ tarbell/cli.py
@@ -10,12 +10,17 @@ This module provides the CLI interface to legit.
 import os
 import sys
 from subprocess import call
+import subprocess
 from time import sleep
 
 import clint.resources
 from clint import args
 from clint.eng import join as eng_join
 from clint.textui import colored, puts, columns
+
+from legit.cli import cmd_switch
+
+from .app import TarbellSite
 
 #from .core import __version__
 #from .settings import settings
@@ -101,17 +106,50 @@ def show_error(msg):
     sys.stderr.write(msg + '\n')
 
 
-# @TODO wrap with decorator forcing active env
-def cmd_newproject(args):
-    print "new project"
+def ensure_site(fn, path=None):
+    def new_ensure_site(args):
+        return fn(args, path)
+
+    if not path:
+        path = os.getcwd()
+
+    if path is "/":
+        show_error("The current directory is not part of a Tarbell project")
+        sys.exit(1)
+
+    if not os.path.exists(os.path.join(path, '.tarbell')):
+        path = os.path.realpath(os.path.join(path, '..'))
+        return ensure_site(fn, path)
+    else:
+        os.chdir(path)
+        return new_ensure_site
 
 
-def cmd_serve(args):
-    print "serve"
+@ensure_site
+def tarbell_newproject(args, path):
+    project = args.get(0)
+    if project:
+        print "@todo create new project {0}".format(project)
+    else:
+        show_error("No project name specified")
+
+@ensure_site
+def tarbell_serve(args, path):
+    path_parts = path.split('/')
+    ip = args.get(0)
+    server_path = os.path.abspath(os.path.dirname(__file__))
+    if not ip: ip = '127.0.0.1'
+    script = "source ~/.virtualenvs/{root}/bin/activate && \
+            python {server_path}/runserver.py {path} {ip}" \
+            .format(root=path_parts[-1], server_path=server_path, path=path, ip=ip)
+    print script
+    process = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process.communicate(script)
 
 
-def cmd_switch(args):
-    print "switch"
+def tarbell_switch(args):
+    cmd_switch(args)    # legit switch
+    tarbell_serve(args) # serve 'em up!
 
 
 class Command(object):
@@ -157,20 +195,20 @@ def def_cmd(name=None, short=None, fn=None, usage=None, help=None):
 
 def_cmd(
     name='newproject',
-    fn=cmd_newproject,
+    fn=tarbell_newproject,
     usage='newproject <project>',
     help='Create a new project named <project>')
 
 
 def_cmd(
     name='serve',
-    fn=cmd_serve,
+    fn=tarbell_serve,
     usage='serve',
     help='Run a preview server (typically handled by `switch`)')
 
 
 def_cmd(
     name='switch',
-    fn=cmd_switch,
+    fn=tarbell_switch,
     usage='switch <project>',
     help='Switch to the project named <project> and start a preview server.')
