76543fb77b95aa3a4018142e6f2d0af74011f824
David Eads <davideads@gmail.com>
2013-08-28 23:01:18 -0500

    setup changes to reflect legit style

--- setup.py
+++ setup.py
@@ -1,23 +1,65 @@
-from distutils.core import setup
-from setuptools import find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
 
-setup(
-    name='tarbell',
-    version='0.8',
-    author=u'David Eads',
-    author_email='davideads@gmail.com',
-    packages=find_packages(),
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+APP_NAME = 'tarbell'
+APP_SCRIPT = './tarbell_runner'
+VERSION = '0.8'
+
+settings = dict()
+
+# Grab requirments.
+with open('requirements.txt') as f:
+    required = f.readlines()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+# Build Helper.
+if sys.argv[-1] == 'build':
+    try:
+        import py2exe
+    except ImportError:
+        print 'py2exe is required to continue.'
+        sys.exit(1)
+
+    sys.argv.append('py2exe')
+
+    settings.update(
+        console=[{'script': APP_SCRIPT}],
+        zipfile = None,
+        options = {
+            'py2exe': {
+                'compressed': 1,
+                'optimize': 0,
+                'bundle_files': 1}})
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    author=u'Chicago Tribune News Applications Team',
+    author_email='newsapps@tribune.com',
+    packages=['tarbell',],
     url='http://github.com/newsapps/tarbell',
-    license='MIT, see LICENSE.md',
-    description= 'A very simple web authoring tool.',
+    license='MIT',
+    description= 'A very simple content management system',
     long_description=open('README.md').read(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[
-        'Flask>=0.9',
-        'boto>=2.3.0',
-        'requests>=0.11.2',
-        'gdata>=2.0.17',
-        'ordereddict>=1.1',
-    ]
+    install_requires=required,
 )
+
+
+setup(**settings)
+
+
