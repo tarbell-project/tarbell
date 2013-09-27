3eae45a68bc8febc50a877dc85f2a0a5e7c3acd0
Ryan Mark <ryan@mrk.cc>
2013-09-27 14:07:06 -0500

    ignore build files, adjust setup.py to include datafiles n such

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import os
 import sys
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 
 APP_NAME = 'tarbell'
@@ -24,38 +24,31 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-
-# Build Helper.
-if sys.argv[-1] == 'build':
-    try:
-        import py2exe
-    except ImportError:
-        print 'py2exe is required to continue.'
-        sys.exit(1)
-
-    sys.argv.append('py2exe')
-
-    settings.update(
-        console=[{'script': APP_SCRIPT}],
-        zipfile = None,
-        options = {
-            'py2exe': {
-                'compressed': 1,
-                'optimize': 0,
-                'bundle_files': 1}})
-
 settings.update(
     name=APP_NAME,
     version=VERSION,
     author=u'Chicago Tribune News Applications Team',
     author_email='newsapps@tribune.com',
-    packages=['tarbell',],
     url='http://github.com/newsapps/tarbell',
     license='MIT',
-    description= 'A very simple content management system',
+    description='A very simple content management system',
     long_description=open('README.md').read(),
     zip_safe=False,
-    include_package_data=True,
+    packages=find_packages(),
+    data_files=[
+        ('tarbell/tests/example/base',
+            ['tarbell/tests/example/base/config.py']),
+        ('tarbell/tests/example/base/static',
+            ['tarbell/tests/example/base/static/style.css']),
+        ('tarbell/tests/example/base/templates',
+            ['tarbell/tests/example/base/templates/_base.html']),
+        ('tarbell/tests/example/project',
+            ['tarbell/tests/example/project/config.py']),
+        ('tarbell/tests/example/project/static',
+            ['tarbell/tests/example/project/static/style.css']),
+        ('tarbell/tests/example/project/templates',
+            ['tarbell/tests/example/project/templates/index.html']),
+    ],
     install_requires=required,
     entry_points={
         'console_scripts': [
@@ -66,5 +59,3 @@ settings.update(
 
 
 setup(**settings)
-
-
