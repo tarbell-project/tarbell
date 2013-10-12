d3e3fa53e9364ba9ba90eb0b7fee009ceca204e3
David Eads <davideads@gmail.com>
2013-10-12 00:06:44 -0500

    make get_config_from_args work correctly

--- tarbell/utils.py
+++ tarbell/utils.py
@@ -51,18 +51,20 @@ def show_error(msg):
 
 def get_config_from_args(args):
     """Get config directory from arguments."""
-
     if args.contains('--settings'):
-        # @TODO handle
-        return "settingsdir"
-
+        path = args.value_after("--settings")
+        if not os.path.isdir(path):
+            show_error("{0} does not exist. Exiting...".format(
+                colored.cyan(path)
+            ))
+            sys.exit()
+        return path
     elif sys.platform == 'win32':
         return os.path.join(os.environ['APPDATA'], "Tarbell")
-
     else:
-        return os.path.expanduser(os.path.join("~",
-                                               ".{0}".format("tarbell")
-                                               ))
+        return os.path.expanduser(
+            os.path.join("~", ".{0}".format("tarbell"))
+        )
 
 def filter_files(path):
     for dirpath, dirnames, filenames in os.walk(path):
