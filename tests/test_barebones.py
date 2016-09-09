fa55e9ae88cbf2b8e8acf288d0573a0b37be6b31
Chris Amico <eyeseast@gmail.com>
2016-09-09 15:37:06 -0400

    Better directory comparison

--- tests/test_barebones.py
+++ tests/test_barebones.py
@@ -2,6 +2,7 @@
 """
 Tests for the barebones example project
 """
+import filecmp
 import os
 import py.path
 
@@ -9,13 +10,15 @@ from tarbell.app import EXCLUDES, TarbellSite
 
 TESTS_DIR = os.path.dirname(__file__)
 PATH = os.path.realpath(os.path.join(TESTS_DIR, 'examples/barebones'))
+BUILT = os.path.join(PATH, '_site')
 
+PROJECT_NAME = "barebones"
 
 def test_get_site():
     site = TarbellSite(PATH)
 
     assert os.path.realpath(site.path) == os.path.realpath(PATH)
-    assert site.project.name == "barebones"
+    assert site.project.name == PROJECT_NAME
 
 
 def test_default_excludes():
@@ -28,11 +31,10 @@ def test_default_excludes():
 def test_generate_site(tmpdir):
     "Generate a static site matching what's in _site"
     site = TarbellSite(PATH)
-    built = os.path.join(PATH, '_site')
 
     site.generate_static_site(str(tmpdir))
 
-    files = set(f.basename for f in tmpdir.listdir())
+    # compare directories
+    comp = filecmp.dircmp(BUILT, str(tmpdir))
 
-    assert files == set(['data.json', 'index.html'])
-
+    assert set(comp.same_files) == set(os.listdir(BUILT))
