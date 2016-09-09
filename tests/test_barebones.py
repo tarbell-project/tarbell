3c08a57879da5683d6a125a148f36534644c8bfb
Chris Amico <eyeseast@gmail.com>
2016-09-09 12:00:51 -0400

    Basic tests, one failing

---
+++ tests/test_barebones.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for the barebones example project
+"""
+import os
+import py.path
+from tarbell.app import TarbellSite
+
+PATH = os.path.realpath('examples/barebones')
+
+def test_get_site():
+    site = TarbellSite(PATH)
+
+    assert os.path.realpath(site.path) == os.path.realpath(PATH)
+    assert site.project.name == "barebones"
+
+
+def test_generate_site(tmpdir):
+    "Generate a static site matching what's in _site"
+    site = TarbellSite(PATH)
+    built = os.path.join(PATH, '_site')
+
+    site.generate_static_site(str(tmpdir))
+
+    files = set(f.basename for f in tmpdir.listdir())
+
+    assert files == set(['data.json', 'index.html'])
