2f0d80928417ace2c231687fc5b624f8c9721dac
Chris Amico <eyeseast@gmail.com>
2017-07-31 13:45:53 -0400

    Use test_build_site.py to test that a given site will build

--- tests/test_build_site.py
+++ tests/test_build_site.py
@@ -4,37 +4,34 @@ Suite of tests that ensure an example project is built to spec.
 Given a project with a pre-built _site directory, each test should
 build the site and make sure it matches the pre-built version.
 """
-
 import filecmp
 import os
-import py.path
+from glob import iglob
 
-from tarbell.app import EXCLUDES, TarbellSite
+import pytest
 
-class SiteTest(object):
+from tarbell.app import TarbellSite
+
+
+EXAMPLES_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), 'examples'))
+TEST_SITES = [os.path.join(EXAMPLES_DIR, d) for d in os.listdir(EXAMPLES_DIR)]
+
+
+@pytest.mark.parametrize('path', TEST_SITES)
+def test_site(path, tmpdir):
     """
-    A Tarbell site to test. It should have a pre-built _site directory.
+    Test that sites in the examples/ directory build correctly
     """
-    def __init__(self, path, name):
-        self.path = py.path(path)
-        self.name = name
-        self.site = TarbellSite(str(self.path))
+    site = TarbellSite(path)
 
-    def test_get_site(self):
-        assert os.path.realpath(self.site.path) == os.path.realpath(self.path)
-        assert self.site.project.name == self.name
+    assert os.path.realpath(site.path) == os.path.realpath(path)
 
-    def test_default_excludes(self):
-        "Ensure a basic set of excluded files"
-        assert set(self.site.project.EXCLUDES) == set(EXCLUDES)
+    # build the site
+    site.generate_static_site(str(tmpdir))
 
+    # compare directories
+    built = os.path.join(path, '_site')
+    comp = filecmp.dircmp(built, str(tmpdir))
 
-    def test_generate_site(self, tmpdir):
-        "Generate a static site matching what's in _site"
-        self.site.generate_static_site(str(tmpdir))
+    assert set(comp.same_files) == set(os.listdir(built))
 
-        # compare directories
-        built = os.path.join(self.path, '_site')
-        comp = filecmp.dircmp(built, str(tmpdir))
-
-        assert set(comp.same_files) == set(os.listdir(built))
